0a848dd532b68d6ab018189560e0d7c377ef458f
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-07-11 11:47:14 +0700

    add: header

--- client/src/components/Home/Header/HomeHeader.tsx
+++ client/src/components/Home/Header/HomeHeader.tsx
@@ -4,7 +4,14 @@ import './HomeHeader.css'
 const HomeHeader = () =>{
     return(
         <div className="Home__header">
-            <h1>VN-UK</h1>
+            <div className="row">
+                <h1 className="logo">VN-UK</h1>
+                <div className="border"></div>
+                <div className="col">
+                    <p className="name-logo">Institute for Research</p>
+                    <p className="name-logo"> & Executive Education</p>
+                </div>
+            </div>
         </div>
     )
 }
