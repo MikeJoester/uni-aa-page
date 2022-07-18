71c1636e6728dd57eb296f55e49608d697639640
Harry <vuth@bap.jp>
2022-07-18 15:54:13 +0700

    feat: backend initial commit

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+.then(console.log("Connected to MongoDB"))
+.catch(err => console.log(err));
+
+
+// route goes here
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
