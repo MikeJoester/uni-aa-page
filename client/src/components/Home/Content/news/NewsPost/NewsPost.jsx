711b60a43f841df75e87725d33e2facb1663986a
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-07-23 23:17:31 +0700

    fix: news-res

--- client/src/components/Home/Content/news/NewsPost/NewsPost.jsx
+++ client/src/components/Home/Content/news/NewsPost/NewsPost.jsx
@@ -7,12 +7,12 @@ const news = ({
     title,
     }) => {
   return (
-    <div className="content__article-row">
-      <a href={link}>
-        <div className="new__block">
+    <div className="new__block">
+      <a href={link} className="new__block-links">
+        {/* <div className="new__block"> */}
               <img className="new__img" src={image} alt=""/>
               <p className="new__title">{title}</p> 
-        </div>
+        {/* </div> */}
       </a>
     </div>
   )
