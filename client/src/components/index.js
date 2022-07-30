d3ca393d6ae6a7556ee3cb764b5191d5dd213d5f
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-07-30 12:28:09 +0700

    change file index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,9 @@
-import Navbar from "./Navbar/Navbar";
-// import Sidebar from "./Sidebar/Sidebar";
+// import Navbar from "./Navbar/Navbar";
+import Sidebar from "./Sidebar/Sidebar";
+import Hamburger from "./Hamburger/Hamburger";
+import NewsNav from "./Sidebar/Content/NewsNav/NewsNav";
+import MenuNav from "./Sidebar/Content/MenuNav/MenuNav";
+import LoginNav from "./Sidebar/Content/LoginNav/LoginNav";
 
 //Home
 import HomeHeader from "./Home/Header/HomeHeader";
@@ -10,29 +14,41 @@ import HomeExplore from "./Home/Content/Explore/explore";
 import HomeEvents from "./Home/Content/Events/events";
 import HomeMemory from "./Home/Content/memory/memory";
 import HomeSearch from "./Home/Content/search/search";
-//NewsPage
-import NewHeader from "./NewsPages/NewHeader/NewHeader";
-import NewsBody from "./NewsPages/NewsBody/NewsBody";
-//Survey
-import SurveyHeader from "./Survey/SurveyHeader/SurveyHeader";
-import SurveyBody from "./Survey/SurveyBody/SurveyBody";
-//Rules
-import RulesHeader from "./Rules/RulesHeader/RulesHeader";
-import RulesBody from "./Rules/RulesBody/RulesBody";
 
+//Curriculum
+import CurriculumHeader from "./Curriculum/CurriculumHeader/CurriculumHeader";
+import CurriculumContent from "./Curriculum/CurriculumContent/CurriculumContent";
+import CurriDetailsHeader from "./Curriculum/CurriculumDetails/CurriDetailsHeader/CurriDetailsHeader";
+import CurriDetailsContent from "./Curriculum/CurriculumDetails/CurriDetailsContent/CurriDetailsContent";
+// import CurriCatalogue from "./Curriculum/CurriculumDetails/CurriCatalogue/CurriCatalogue";
 
+//Notification form Lecturer
+import InformContent from "./InformLecturer/InformContent/InformContent";
+import InformHeader from "./InformLecturer/InformHeader/InformHeader";
+import InformTable from "./InformLecturer/InformTable/InformTable";
+
+//Transcript
+import StudentResult from "./StudentTranscript/StudentResult/StudentResult";
+import StudentHeader from "./StudentTranscript/TranscriptHeader/StudentHeader";
+import StudentContent from "./StudentTranscript/StudentContent/StudentContent";
+
+//Personal information
+import PersonalHeader from "./PersonalInfo/PersonalHeader/PersonalHeader";
+import PersonalContent from "./PersonalInfo/PersonalContent/PersonalContent";
+
+//Credit register
+import RegisterCreditContent from "./RegisterCredit/RegisterCreditContent/RegisterCreditContent";
+import RegisterHeader from "./RegisterCredit/RegisterHeader/RegisterHeader";
 //Footer
 import Footer from "./Footer/Footer";
 
-//Admin
-import AdminNavbar from "./Admin/Navbar/AdminNavbar";
-import StudentList from "./Admin/StudentList/StudentList";
-import DashboardMain from "./Admin/DashboardMain/DashboardMain";
-import BlogList from "./Admin/BlogList/BlogList";
-
 export {
-    Navbar,
-    // Sidebar,
+    // Navbar,
+    Sidebar,
+    NewsNav,
+    MenuNav,
+    LoginNav,
+    Hamburger,
     HomeHeader,
     NewsPost,
     HomeContent,
@@ -41,17 +57,19 @@ export {
     HomeEvents,
     HomeMemory,
     HomeSearch,
+    CurriculumHeader,
+    CurriculumContent,
     Footer,
-    //Admin
-    AdminNavbar,
-    BlogList,
-    StudentList,
-    DashboardMain,
-    NewHeader,
-    NewsBody,
-    SurveyHeader,
-    SurveyBody,
-    RulesHeader,
-    RulesBody
-
+    CurriDetailsHeader,
+    CurriDetailsContent,
+    InformHeader,
+    InformContent,
+    InformTable,
+    StudentResult,
+    StudentContent,
+    StudentHeader,
+    PersonalHeader,
+    PersonalContent,
+    RegisterCreditContent,
+    RegisterHeader,
 }
